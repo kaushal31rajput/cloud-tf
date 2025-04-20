2d88bf7f082eab68d419bd55ea028b75e3a4f127
kaushal31rajput <kaushal31rajput@gmail.com>
2025-04-20 09:01:50 +0530

    pl

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "👋 Hello from Docker and Kubernetes, you are rockstar!"
+    return "👋 Hello from Docker and Kubernetes, hows training goining on , today is 13th April, sunday! "
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
